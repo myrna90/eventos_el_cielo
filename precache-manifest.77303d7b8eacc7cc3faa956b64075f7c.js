9113f951b6fc571cd45df934412b2476f27c4ff7
Myrna <myrnamares90@gmail.com>
2019-12-03 12:48:59 -0600

    Updates

---
+++ precache-manifest.77303d7b8eacc7cc3faa956b64075f7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b099d371c5ba3b12a229e5b4fdf831",
+    "url": "/myrna90/eventos_el_cielo.git/index.html"
+  },
+  {
+    "revision": "2e5e805b3f976f971ece",
+    "url": "/myrna90/eventos_el_cielo.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "51e4afd511541241aa98",
+    "url": "/myrna90/eventos_el_cielo.git/static/js/2.3236ccb9.chunk.js"
+  },
+  {
+    "revision": "2e5e805b3f976f971ece",
+    "url": "/myrna90/eventos_el_cielo.git/static/js/main.8eaa4214.chunk.js"
+  },
+  {
+    "revision": "006b38f886122a50b12b",
+    "url": "/myrna90/eventos_el_cielo.git/static/js/runtime-main.5073e3be.js"
+  }
+]);
